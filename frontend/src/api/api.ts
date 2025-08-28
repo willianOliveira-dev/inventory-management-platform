f48a83f9091e9505ea72a9a28b80af8b4db2358f
Willian Oliveira <willian.dev.tech@gmail.com>
2025-08-28 10:44:15 -0300

    Configura comunicação com o servidor e protege de futuros errors.

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,29 +1,53 @@
-import axios, { type AxiosError, type AxiosInstance, type AxiosRequestConfig } from 'axios';
+import axios, {
+    type AxiosError,
+    type AxiosInstance,
+    type AxiosRequestConfig,
+} from 'axios';
+import TokenStorage from '../utils/TokenStorage';
 
 const api: AxiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
     withCredentials: true,
 });
 
+// Automaticamente coloca o token de autenticação em todas as requisições feitas para o servidor
+
+api.interceptors.request.use(
+    // interceptar todas as requisições  HTTP para o Servidor
+    (config) => {
+        // Função que roda antes da requisição
+        if (TokenStorage.hasToken()) {
+            //
+            const token: string | null = TokenStorage.getToken();
+            config.headers.Authorization = `Bearer ${token}`; // adiciona no cabeçalho
+        }
+        return config; // Continuar a requisição
+    },
+    (error: AxiosError) => {
+        return Promise.reject(error); // A função que trata errors do interceptor
+    }
+);
+
+// interceptando a resposta antes de chegar ao cliente 
 api.interceptors.response.use(
-    (response) => response,
-    async (error: AxiosError) => {
-        const originalRequest = error.config;
+    (response) => response, // Se deu tudo certo, apenas devolve a resposta
+    async (error: AxiosError) => { // Função de reparo, caso haja error de 403 ou outros
+       const originalRequest = error.config;
         if (error.response?.status === 403 && !originalRequest?._retry) {
             originalRequest!._retry = true;
             try {
-                const { data } = await api.post('/auth/refresh');
-                const newAccessToken = data.data;
+                const { data } = await api.post('panel/v1/auth/refresh');
+                const newAccessToken = data.accessToken;
 
-                originalRequest!.headers[
-                    'Authorization'
-                ] = `Bearer ${newAccessToken}`;
+                TokenStorage.setToken(newAccessToken);
 
+                originalRequest!.headers.Authorization = `Bearer ${newAccessToken}`;
+                
                 return api(originalRequest as AxiosRequestConfig);
 
-            } catch {
-                console.log('Sessão expirada, faça login novamente');
-                return Promise.reject(error);
+            } catch(refreshError) {
+                TokenStorage.clearToken();
+                return Promise.reject(refreshError);
             }
         }
         return Promise.reject(error);
