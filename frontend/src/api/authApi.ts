56cefc2a0bcb12782350a42bad33a909458e6018
Willian Oliveira <willian.dev.tech@gmail.com>
2025-08-28 13:39:00 -0300

    feat(auth): Cria a base de autenticação

---
+++ frontend/src/api/authApi.ts
@@ -0,0 +1,25 @@
+import type { LoginData, RegisterData, AuthResponse } from '../types';
+import TokenStorage from '../utils/TokenStorage';
+import api from './api';
+
+export const authApi = {
+    async login(data: LoginData): Promise<AuthResponse> {
+        const { data: response } = await api.post<AuthResponse>('/panel/v1/auth/login', data );
+        return response;
+    },
+
+    async register(data: RegisterData): Promise<AuthResponse> {
+        const { data: response } = await api.post('/panel/v1/auth/register', data);
+        return response;
+    },
+
+    async logout(): Promise<void> {
+        await api.post('/panel/v1/auth/logout');
+        TokenStorage.clearToken();
+    },
+
+    async refresh(): Promise<AuthResponse> {
+        const {data:response} = await api.post('/panel/v1/auth/refresh');
+        return response;
+    }
+};
