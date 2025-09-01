e51afea0896c14e4ef48bf1013624da9712ed49c
Willian Oliveira <willian.dev.tech@gmail.com>
2025-09-01 10:33:20 -0300

    feat(router): constroi a base para usar as rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,11 @@
+import AuthProvider from './providers/AuthProvider';
+import router from './router/router';
+import { RouterProvider } from 'react-router-dom';
+
 export default function App() {
-    return <></>;
+    return (
+        <AuthProvider>
+            <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
+    );
 }
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -1,13 +1,7 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
 // react-router-dom - biblioteca vai criar o roteador
-// create-router-dom - simular o uso do navegador, 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: "/"
-    }
-])
-
+// create-router-dom - simular o uso do navegador,
+const router = createBrowserRouter([]);
 
 export default router;
