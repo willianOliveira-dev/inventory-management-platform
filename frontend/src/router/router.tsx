9e4ae9dadcf8f0a8c2797441bd53495eedae6c6e
Willian Oliveira <willian.dev.tech@gmail.com>
2025-09-01 10:05:32 -0300

    chore(package): instala o tailwindcss e adiciona o plugin nas configurações do vite

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+// react-router-dom - biblioteca vai criar o roteador
+// create-router-dom - simular o uso do navegador, 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: "/"
+    }
+])
+
+
+export default router;
