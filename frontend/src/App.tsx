ff4c66b2fcf090e0a8b72dac71a3033cf1206a37
Willian Oliveira <willian.dev.tech@gmail.com>
2025-09-04 09:12:00 -0300

    chore(package): instala a biblioteca AOS para animações de scroll e aplica no App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import AOS from 'aos';
 import AuthProvider from './providers/AuthProvider';
 import router from './router/router';
 import { RouterProvider } from 'react-router-dom';
+import 'aos/dist/aos.css';
 
 export default function App() {
+    AOS.init();
     return (
         <AuthProvider>
             <RouterProvider router={router}></RouterProvider>
